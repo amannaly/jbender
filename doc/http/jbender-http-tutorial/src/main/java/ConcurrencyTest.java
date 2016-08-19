0765eae370b12fc7ee7a56506a50234a0372a5e7
Arjun Mannaly <a.mannaly@gmail.com>
2016-08-18 23:01:46 -0700

    Test for echo endpoint.

---
+++ doc/http/jbender-http-tutorial/src/main/java/ConcurrencyTest.java
@@ -0,0 +1,82 @@
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.SuspendExecution;
+import co.paralleluniverse.strands.channels.Channel;
+import co.paralleluniverse.strands.channels.Channels;
+import com.pinterest.jbender.JBender;
+import com.pinterest.jbender.events.TimingEvent;
+import com.pinterest.jbender.events.recording.HdrHistogramRecorder;
+import com.pinterest.jbender.executors.http.FiberApacheHttpClientRequestExecutor;
+import org.HdrHistogram.Histogram;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.nio.reactor.IOReactorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import static com.pinterest.jbender.events.recording.Recorder.record;
+
+public class ConcurrencyTest {
+
+    private static final int noOfReq = 1_000_000;
+    private static final int warmUpReq = 0;
+    private static final int concurrency = 1000;
+    private static final int queueSize = 100_000;
+
+    public static void main(final String[] args) throws SuspendExecution, InterruptedException, ExecutionException, IOReactorException, IOException {
+
+        String url = "http://ec2-52-43-251-21.us-west-2.compute.amazonaws.com/echo";
+
+        try (final FiberApacheHttpClientRequestExecutor requestExecutor =
+                     new FiberApacheHttpClientRequestExecutor<>((res) -> {
+                         if (res == null) {
+                             throw new AssertionError("Response is null");
+                         }
+                         final int status = res.getStatusLine().getStatusCode();
+                         if (status != 200) {
+                             throw new AssertionError("Status is " + status);
+                         }
+                     }, 10_000)) {
+
+            final Channel<HttpGet> requestCh = Channels.newChannel(queueSize);
+            final Channel<TimingEvent<CloseableHttpResponse>> eventCh = Channels.newChannel(queueSize);
+
+            // Requests generator
+            new Fiber<Void>("req-gen", () -> {
+                // Bench handling 1k reqs
+                for (int i = 0; i < noOfReq; ++i) {
+                    requestCh.send(new HttpGet(url));
+                }
+
+                requestCh.close();
+            }).start();
+
+            final Histogram histogram = new Histogram(10000L, 3);
+
+            // Event recording, both HistHDR and logging
+            HdrHistogramRecorder hdrHistogramRecorder = new HdrHistogramRecorder(histogram, 1_000_000);
+            record(eventCh, hdrHistogramRecorder);
+
+            long startTime = System.currentTimeMillis();
+            new Fiber<Void>("jbender", () -> {
+                // with 5000 warmup requests.
+                JBender.loadTestConcurrency(concurrency, warmUpReq, requestCh, requestExecutor, eventCh);
+            }).start().join();
+
+            double duration = (System.currentTimeMillis() - startTime) / 1000.0;
+            //histogram.outputPercentileDistribution(System.out, 1.0);
+
+            LOG.info("Number of requests: {}", histogram.getTotalCount());
+            LOG.info("Throughput: {}", histogram.getTotalCount()/duration);
+            LOG.info("Number of errors: {}", hdrHistogramRecorder.errorCount);
+            LOG.info("Mean response time: {} ms", histogram.getMean());
+            LOG.info("95th percentile response time: {} ms", histogram.getValueAtPercentile(95.0));
+            LOG.info("99th percentile response time: {} ms", histogram.getValueAtPercentile(99.0));
+            LOG.info("Max response time: {} ms", histogram.getMaxValueAsDouble());
+        }
+    }
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoadTest.class);
+}
